9c46800368a92440517918466b5f94ce8086cf94
Florian Macherey <f.macherey@fz-juelich.de>
2015-04-02 16:28:32 +0200

    cleaned imports

--- src/BookClient.java
+++ src/BookClient.java
@@ -1,10 +1,7 @@
-import java.io.BufferedWriter;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
