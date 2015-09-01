34b3c6072f391848f563f4f0d6b58df21631b6bc
Florian Macherey <f.macherey@fz-juelich.de>
2015-09-01 09:35:02 +0200

    Hallo Datei fuer C Tutorium hinzugefuegt

---
+++ c_tutorium/hallo.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>*/
+/*#include <stdlib.h>*/
+
+int main() {
+    int a=5;
+    unsigned int b=-3;
+    int c;
+   
+    a += b;
+
+    if (b > a) {
+        printf("Falsch\n");
+    } else {
+        printf("Hallo 2014er MaTSEs\n");
+    }
+
+    return 0;
+}
